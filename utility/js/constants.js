2ffdd899cf22d29e5da41a6d0f4778130b7ec608
mindhack <mindhack.in.games@gmail.com>
2025-11-08 22:11:46 +0530

    constants revert

--- utility/js/constants.js
+++ utility/js/constants.js
@@ -1,6 +1,6 @@
 
-// export const url="https://www.mindhack.in/"
+export const url="https://www.mindhack.in/"
 // export const dynamicUrl="https://dynamic.mindhack.in/";
 
 // export const dynamicUrl="http://127.0.0.1:5502/";
-export const url="http://127.0.0.1:5501/";
+// export const url="http://127.0.0.1:5501/";
