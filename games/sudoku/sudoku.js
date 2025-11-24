01ec7132a81e3a8d54d4264036b914aaf0a8655e
mindhack <mindhack.in.games@gmail.com>
2025-11-25 00:02:24 +0530

    sudoku upgrade

--- games/sudoku/sudoku.js
+++ games/sudoku/sudoku.js
@@ -11,7 +11,7 @@ async function createSudokuGrid(difficulty = "easy") {
   mistakes = 3;
   const response =
 
-    fetch(dynamicUrl + `/games/sudoku/${difficulty}.json`)
+    fetch(dynamicUrl + `games/sudoku/${difficulty}.json`)
       .then(response => response.json())
       .then(data => {
         const length = data.length;
