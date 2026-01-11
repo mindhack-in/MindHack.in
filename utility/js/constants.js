ae3011a9478f25b9857656f5f90565fe00391a8f
mindhack <mindhack.in.games@gmail.com>
2026-01-11 20:39:09 +0530

    hotfix

--- utility/js/constants.js
+++ utility/js/constants.js
@@ -1,6 +1,6 @@
 
-// export const url="https://www.mindhack.in/"
+export const url="https://www.mindhack.in/"
 
-export const url="http://127.0.0.1:5501/";
+// export const url="http://127.0.0.1:5501/";
 export const beurl="https://mindhack-in-backend-137262061877.asia-south1.run.app/"
 // export const beurl="http://localhost:9090/"
